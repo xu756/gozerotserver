d2af08366017a2c8ebb00368b95d014f907debb6
xu756 <756334744@qq.com>
2022-12-25 23:55:56 +0800

    修改逻辑

--- app/user/api/internal/logic/userloginlogic.go
+++ app/user/api/internal/logic/userloginlogic.go
@@ -2,6 +2,8 @@ package logic
 
 import (
 	"context"
+	"github.com/golang-jwt/jwt/v4"
+	"time"
 
 	"gozerotserver/app/user/api/internal/svc"
 	"gozerotserver/app/user/api/internal/types"
@@ -22,9 +24,24 @@ func NewUserLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserLog
 		svcCtx: svcCtx,
 	}
 }
-
+func (l *UserLoginLogic) getJwtToken(secretKey string, iat int64, seconds int64, username string) (string, error) {
+	claims := make(jwt.MapClaims)
+	claims["exp"] = iat + seconds
+	claims["iat"] = iat
+	claims["user"] = username
+	token := jwt.New(jwt.SigningMethodHS256)
+	token.Claims = claims
+	return token.SignedString([]byte(secretKey))
+}
 func (l *UserLoginLogic) UserLogin(req *types.UserLogin) (resp *types.Res, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+	now := time.Now().Unix()
+	jwtToken, e := l.getJwtToken(l.svcCtx.Config.Auth.AccessSecret, now, l.svcCtx.Config.Auth.AccessExpire, req.Username)
+	if e != nil {
+		return nil, err
+	}
+	return &types.Res{
+		Code: 200,
+		Msg:  "success",
+		Data: jwtToken,
+	}, nil
 }
